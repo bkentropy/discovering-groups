aea500acb1fb62bc9f2383972035ee04c1722efc
Brian Kustra <bkustrakb@gmail.com>
2018-02-10 17:47:22 -0600

    reorganized, and got top level comment ids

--- redditconnect.py
+++ redditconnect.py
@@ -10,27 +10,39 @@ reddit = praw.Reddit(
     username=auth["username"]
 )
 
-# reddit.subreddit('test').submit("Hello worlllllld", url="https://reddit.com")
-# submission = reddit.submission(url="https://www.reddit.com/r/test/comments/5vg5d8")
-# submission.reply("Super rad")
-# for s in reddit.front.hot(limit=256):
-#    print(s.score)
+def getTopLevelComment(submissions):
+    top_lvl_cmts = []
+    for sub in submissions:
+        top_lvl_cmt = [cmt for cmt in sub.comments]
+        top_lvl_cmts.append(top_lvl_cmt)
+    return top_lvl_cmts
 
-prog = reddit.subreddit('programming')
 
-# this gets 25 ish posters
-user_dict = {}
-
-submissions = [sub for sub in prog.hot(limit=25)]
-for sub in submissions:
-    user_dict[sub.author] = {}
-
-for sub in submissions:
-    top_lvl_cmts = [cmt for cmt in sub.comments]
-
-def usernameToTitle(submissions):
+def getUsers(submissions):
     return [{"username": sub.author.name, "title": sub.title}
             for sub in submissions]
 
-untotitle = username(submissions)
+def main():
+    # Example use
+    prog = reddit.subreddit('programming')
 
+    # this will get 25 posters
+    submissions = [sub for sub in prog.hot(limit=25)]
+
+    #user_dict = getUsers(submissions)
+    #print("User name to title hash")
+    #print(user_dict)
+
+    # this actually produces an array of arrays 
+    # that contain comment ids it looks like
+    # [Comment(id='dtyuazw'), Comment(id='dtxqs3d')], [Comment(id='dtxkhdc'),
+    # Comment(id='dtxjmoe')], [Comment(id='dtyoivu')],
+    # [Comment(id='dty8zyf')], [], [], [], [], [Comment(id='dtxigt3'),
+    # Comment(id='dtxz8fu')], [Comment(id='dty823n')],
+    # [Comment(id='dtyb1qx')], [], []]
+    toplvl = getTopLevelComment(submissions)
+    print('Top level comments of all of the submissions:')
+    print(toplvl)
+
+if __name__ == "__main__":
+    main()
